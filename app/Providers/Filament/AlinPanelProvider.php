b786b61dc6e3d7acce3c6b53c6a7d32bba0ce473
alin-comcity <1000957@daffodil.ac>
2025-02-19 13:14:14 +0600

    modify primary color

--- app/Providers/Filament/AlinPanelProvider.php
+++ app/Providers/Filament/AlinPanelProvider.php
@@ -30,7 +30,7 @@ class AlinPanelProvider extends PanelProvider
             ->path('alin')
             ->login()
             ->colors([
-                'primary' => Color::Red,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
